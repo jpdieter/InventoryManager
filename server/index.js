006c67a91dffe0e0d1c5b8b0025eddc02d94c00e
jpdieter <justindieter@gmail.com>
2024-05-02 21:23:38 -0700

    added search route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const pool = require('./db');
 const cors = require('cors');
 require('dotenv').config()
 const productRouter = require('./routes/products');
+const searchRouter = require('./routes/search');
 
 // Your application code goes here
 
@@ -11,6 +12,7 @@ const productRouter = require('./routes/products');
 app.use(cors());
 app.use(express.json());
 app.use('/products', productRouter)
+app.use('/search', searchRouter)
 
 app.listen(4000, () =>{
     console.log('Server has started on port 4000!')
