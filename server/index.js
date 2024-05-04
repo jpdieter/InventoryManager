1d234c9d783faeb933e8ddf33940270495c8ee60
jpdieter <justindieter@gmail.com>
2024-05-04 10:45:31 -0700

    added Helmet middleware to protect against well-known web vulnerabilities

--- server/index.js
+++ server/index.js
@@ -6,17 +6,28 @@ require('dotenv').config()
 const productRouter = require('./routes/products');
 const searchRouter = require('./routes/search');
 const compression = require("compression");
+const helmet = require('helmet');
 
-// Your application code goes here
-
-//middleware
+//Middleware
 app.use(cors());
 app.use(express.json());
 app.use(compression()); // Compress all routes
+
+app.use(helmet());
+app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'"],
+      },
+    })
+  );
+
+//Routes
 app.use('/products', productRouter)
 app.use('/search', searchRouter)
 
-
+//Start server
 app.listen(4000, () =>{
     console.log('Server has started on port 4000!')
 })
