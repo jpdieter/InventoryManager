192e3e2ca77c745732d45bc77d7f8474c9d00982
jpdieter <justindieter@gmail.com>
2024-05-04 10:02:25 -0700

    added compression middleware

--- server/index.js
+++ server/index.js
@@ -5,14 +5,17 @@ const cors = require('cors');
 require('dotenv').config()
 const productRouter = require('./routes/products');
 const searchRouter = require('./routes/search');
+const compression = require("compression");
 
 // Your application code goes here
 
 //middleware
 app.use(cors());
 app.use(express.json());
+app.use(compression()); // Compress all routes
 app.use('/products', productRouter)
 app.use('/search', searchRouter)
+
 
 app.listen(4000, () =>{
     console.log('Server has started on port 4000!')
