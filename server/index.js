52e296f01d520eea9b1b7a81ee05ade7dbc4864a
jpdieter <justindieter@gmail.com>
2024-05-05 13:23:55 -0700

    testing db with CORS disabled

--- server/index.js
+++ server/index.js
@@ -9,9 +9,19 @@ const compression = require("compression");
 const helmet = require('helmet');
 
 //Middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(compression()); // Compress all routes
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+windowMs: 1 * 60 * 1000, // 1 minute
+max: 40,
+validate: {xForwardedForHeader: false},
+});
+// Apply rate limiter to all requests
+app.use(limiter);    
 
 app.use(helmet()); //Helmet to protect against well-known web vulnerabilities
 app.use(
